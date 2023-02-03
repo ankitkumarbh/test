ffc3b1e5279a7c73f49084e64cfd44b53baf2be4
ankitkumarbh <ankitkumarbh@gmail.com>
2023-02-03 18:28:33 +0530

    Update __main__.py

--- SaitamaRobot/__main__.py
+++ SaitamaRobot/__main__.py
@@ -43,8 +43,7 @@ from telegram.ext import (
     CommandHandler,
     MessageHandler,
 )
-from telegram.ext.dispatcher import DispatcherHandlerStop, run_async
-from telegram.utils.helpers import escape_markdown
+
 
 
 def get_readable_time(seconds: int) -> str:
